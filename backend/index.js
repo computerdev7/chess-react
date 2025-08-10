00dc28f84d2c11848ce6979858351ee931710de4
computerdev7 <upadhyayakash687@gmail.com>
2025-08-10 17:42:57 +0530

    created an api for unique users

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,16 @@ import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
 import cors from "cors"
-import SocketFunc from "./socketconnection.js"
+import SocketFunc from "./socket_connection/socketconnection.js"
 
 let app = express();
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "OPTIONS"] ,
+    allowedHeaders: ["Content-Type"],
+    credentials: true
+}));
 let server = createServer(app)
 let io = new Server(server, {
     cors: {
@@ -13,6 +20,24 @@ let io = new Server(server, {
         credentials: true
     }
 });
+
+
+let userList = new Map();
+
+app.post('/setUser', (req, res) => {
+    let userName = req.body.userName
+    try {
+        let findUser = userList.get(userName)
+        if (!findUser) {
+            userList.set(userName, userName)
+            res.status(201).json({ message: 'user is created' })
+        } else {
+            res.status(400).json({ message: 'username already exist' })
+        }
+    } catch (err) {
+        console.log(err)
+    } 
+})
 
 SocketFunc(io);
 
